6edb0f60ac06b6caf1a1233c43d233b68624898f
Tudor Jianu <jianu.tudor9@gmail.com>
2024-03-27 07:56:25 +0000

    up

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB =
+  "mongodb+srv://tj:tj@cluster0.f95adqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
